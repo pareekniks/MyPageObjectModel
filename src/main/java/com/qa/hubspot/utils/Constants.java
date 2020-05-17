5c149a66f52ffbac176bf3d7913977bb3dd52ceb
grznik18 <Nikhil.Pareek.external@andritz.com>
2020-05-17 18:18:59 +0530

    Test Code

--- src/main/java/com/qa/hubspot/utils/Constants.java
+++ src/main/java/com/qa/hubspot/utils/Constants.java
@@ -2,6 +2,7 @@ package com.qa.hubspot.utils;
 
 public class Constants {
 
+	//This is the file for all constant variables
 	public final static String HOME_PAGE_TITLE = "Reports dashboard";
 	public final static String HOME_PAGE_HEADER = "Sales Dashboard";
 	public final static String LOGIN_PAGE_TITLE = "HubSpot Login";
